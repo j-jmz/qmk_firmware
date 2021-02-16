342207fd9d58798c32b28366bafcebddfbd65d1d
Jacobo Jimenez <jacobo.jimenez@jambit.com>
2021-02-16 12:48:49 +0100

    add dvorak layout

--- keyboards/kyria/keymaps/jacoboj/keymap.c
+++ keyboards/kyria/keymaps/jacoboj/keymap.c
@@ -17,6 +17,7 @@
 
 enum layers {
     _QWERTY = 0,
+    _DVORAK,
     _LOWER,
     _RAISE,
     _ADJUST
@@ -57,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        |      |      | Alt  | Lower| Raise|  | Lower| Raise|      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
-    [_QWERTY] = LAYOUT(
+    [_DVORAK] = LAYOUT(
       LT(_RAISE, KC_ESC),      KC_SCLN, KC_COMM, KC_DOT, KC_P, KC_Y,                                         KC_F,    KC_G,    KC_C,    KC_R,    KC_L, KC_SLSH,
       MT(MOD_LCTL, KC_BSPC),   KC_A,    KC_O,    KC_E,   KC_U, KC_I,                                         KC_D,    KC_H,    KC_T,    KC_N,    KC_S, KC_MINS,
       KC_LSFT,                 KC_QUOT, KC_Q,    KC_J,   KC_K, KC_X,   KC_LSFT,   KC_LGUI, KC_LGUI, KC_LSFT, KC_B,    KC_M,    KC_W,    KC_V,    KC_Z, KC_DLR,
